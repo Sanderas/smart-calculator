ea94f2eecb6a6c6fe425bb54d9f262ed6c7c987e
Alexander <guts-817@rambler.ru>
2018-02-27 19:33:01 +0300

    my desicion

--- src/index.js
+++ src/index.js
@@ -1,27 +1,51 @@
 class SmartCalculator {
-  constructor(initialValue) {
-    // your implementation
-  }
+    constructor(initialValue) {
+        // your implementation
+        this.expression = String(initialValue);
+        this.result = eval(initialValue);
+    }
 
-  add(number) {
-    // your implementation
-  }
-  
-  subtract(number) {
-    // your implementation
-  }
+    add(number) {
+        // your implementation
+        this.expression += ("+" + String(number));
+        this.result = eval(this.expression);
+        return this;
+    }
 
-  multiply(number) {
-    // your implementation
-  }
+    subtract(number) {
+        // your implementation
+        this.expression += ("-" + String(number));
+        this.result = eval(this.expression);
+        return this;
+    }
 
-  devide(number) {
-    // your implementation
-  }
+    multiply(number) {
+        // your implementation
+        this.expression += ("*" + String(number));
+        this.result = eval(this.expression);
+        return this;
+    }
 
-  pow(number) {
-    // your implementation
-  }
+    devide(number) {
+        // your implementation
+        this.expression += ("/" + String(number));
+        this.result = eval(this.expression);
+        return this;
+    }
+
+    pow(number) {
+        // your implementation
+        var coincidence=parseInt(this.expression.match(/\d+\)*$/)[0]);
+        var pow_result='Math.pow(Number('+coincidence+'), '+number+')';
+        var parenthes=this.expression.match(/\)*$/);
+        this.expression=this.expression.replace(/\d+\)*$/, pow_result+parenthes);
+        this.result = eval(this.expression);
+        return this;
+    }
+
+    toString() {
+        return this.result;
+    }
 }
 
 module.exports = SmartCalculator;
